ec9a7c6bb1309c9d2eca89dda970feae63cb1e8e
Danielle Nourok <dnourok1@gmail.com>
2016-06-03 09:28:57 -0400

    first two exercises

---
+++ hello-world/hello-world.js
@@ -0,0 +1,13 @@
+
+var HelloWorld = function() {};
+
+HelloWorld.prototype.hello = function(name) {
+  name = name || 'World';
+  return 'Hello, ' + name + '!';
+};
+
+module.exports = HelloWorld;
+
+
+
+
---
+++ leap/leap.js
@@ -0,0 +1,17 @@
+var Year = function(number) {
+  this.number = number;
+}
+
+Year.prototype.isLeap = function() {
+if (this.number%400==0){
+	return true;
+}else if (this.number%100==0) {
+	return false;
+} else if (this.number%4==0){
+	return true
+} else {
+	return false
+}
+};
+
+module.exports = Year;
