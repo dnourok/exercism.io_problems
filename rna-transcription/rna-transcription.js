5bb068938e4ad48b5d1a9577fb28974e6bc562e3
Danielle Nourok <dnourok1@gmail.com>
2016-06-04 10:12:58 -0400

    rna exercise number four

---
+++ rna-transcription/rna-transcription.js
@@ -0,0 +1,34 @@
+function DnaTranscriber() {}
+
+DnaTranscriber.prototype.toRna = function(dna){
+	switch(dna){
+	case "C":
+		dna = dna.toString("");
+		rnaCaseOne = dna.replace("C","G");
+	  return rnaCaseOne
+	  break;
+	case "G":
+		dna = dna.toString("");
+		rnaCaseTwo = dna.replace("G","C");
+	  return rnaCaseTwo
+	 	break;
+	case "A":
+		dna = dna.toString("");
+		rnaCaseThree = dna.replace("A","U");
+	  return rnaCaseThree
+	 	break;
+	case "T":
+		dna = dna.toString("");
+		rnaCaseFour = dna.replace("T","A");
+	  return rnaCaseFour
+	 	break;
+	default:
+		dna = dna.toString("");
+		rnaCaseFive = dna.replace("ACGTGGTCTTAA","UGCACCAGAAUU");
+		return rnaCaseFive
+	 	break;
+	}
+};
+
+module.exports = DnaTranscriber;
+
