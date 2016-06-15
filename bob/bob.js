fe1196c392d08b2562f89e4f23af8a3e004427d9
Danielle Nourok <dnourok1@gmail.com>
2016-06-15 13:21:57 -0400

    refactored code to pass all of the tests

--- bob/bob.js
+++ bob/bob.js
@@ -1,14 +1,14 @@
-var Bob = function(){};
+var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
-	if (input.includes("-") || input.includes("\'") || input.match("\\d")){
-		return "Whatever."
-	} else if (input.includes("!") || input === input.toUpperCase() || input.toUpperCase().match("\\d")){
-		return "Whoa, chill out!"
-	} else if (input.includes("?")) {
-		return "Sure."
-	} else if (input.match(""))
-		return "Fine. Be that way!"
+    if (input === input.toUpperCase() && input != input.toLowerCase()) {
+        return 'Whoa, chill out!';
+    } else if (input.slice(-1) === "?") {
+        return 'Sure.';
+    } else if (input.trim().length === 0) {
+        return 'Fine. Be that way!';
+    } else 
+    return 'Whatever.';
 };
 
 module.exports = Bob;
